f8fd2bcebf6e5b2bf5f281fbc5df253bdd07cd34
yunhwan <jimmy53120488@gmail.com>
2024-11-09 18:03:05 -0500

    refactor: change types name explicitly

--- app/types/videoTypes.ts
+++ app/types/videoTypes.ts
@@ -1,48 +1,48 @@
-export interface Thumbnail {
+export interface ThumbnailType {
   url: string;
   width: number;
   height: number;
 }
 
-export interface Thumbnails {
-  default: Thumbnail;
-  medium: Thumbnail;
-  high: Thumbnail;
+export interface ThumbnailsType {
+  default: ThumbnailType;
+  medium: ThumbnailType;
+  high: ThumbnailType;
 }
 
-export interface VideoId {
+export interface VideoIdType {
   kind: string;
   videoId: string;
 }
 
-export interface Snippet {
+export interface SnippetType {
   publishedAt: string;
   channelId: string;
   title: string;
   description: string;
-  thumbnails: Thumbnails;
+  thumbnails: ThumbnailsType;
   channelTitle: string;
   liveBroadcastContent: string;
   publishTime: string;
 }
 
-export interface VideoItem {
+export interface VideoItemType {
   kind: string;
   etag: string;
-  id: VideoId;
-  snippet: Snippet;
+  id: VideoIdType;
+  snippet: SnippetType;
 }
 
-export interface PageInfo {
+export interface PageInfoType {
   totalResults: number;
   resultsPerPage: number;
 }
 
-export interface VideoResponse {
+export interface VideoResponseType {
   kind: string;
   etag: string;
   nextPageToken: string;
   regionCode: string;
-  pageInfo: PageInfo;
-  items: VideoItem[];
+  pageInfo: PageInfoType;
+  items: VideoItemType[];
 }
