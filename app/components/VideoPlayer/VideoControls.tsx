6b1b5b63a4fff3526b8500ff25d4145e38b22fd5
yunhwan <jimmy53120488@gmail.com>
2024-11-11 13:55:48 -0500

    docs: update comments on VideoControls.tsx

--- app/components/VideoPlayer/VideoControls.tsx
+++ app/components/VideoPlayer/VideoControls.tsx
@@ -13,6 +13,7 @@ export default function VideoControls({
 }: Props) {
   return (
     <div className="p-1 flex justify-center gap-x-5 bg-slate-300 rounded-xl mb-3">
+      {/* Backward button */}
       <button onClick={onBackwordClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -23,6 +24,7 @@ export default function VideoControls({
           <path d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z" />
         </svg>
       </button>
+      {/* Play/Pause toggle button */}
       <button onClick={onPlaybackClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -45,6 +47,7 @@ export default function VideoControls({
           )}
         </svg>
       </button>
+      {/* Forward button */}
       <button onClick={onForwardClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
